c23bd4e79f4b9be1065a17a7274a94d940945a91
johnobla <45395888+johnobla@users.noreply.github.com>
2020-07-07 13:34:46 +0100

    Added catalog routes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var wiki = require('./routes/wiki');
+var wikiRouter = require('./routes/wiki');
+var catalogRouter = require('./routes/catalog');
 
 //Import the mongoose module
 var mongoose = require('mongoose');
@@ -36,8 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use('/wiki', wiki);
+app.use('/wiki', wikiRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
