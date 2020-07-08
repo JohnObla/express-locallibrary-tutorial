2b4ede6196ccba9fd1090bcb5b2bc88b30be1fed
johnobla <45395888+johnobla@users.noreply.github.com>
2020-07-08 14:58:45 +0100

    Changed date formats

--- models/author.js
+++ models/author.js
@@ -1,48 +1,66 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
   {
-    first_name: {type: String, required: true, maxlength: 100},
-    family_name: {type: String, required: true, maxlength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
+    first_name: { type: String, required: true, maxlength: 100 },
+    family_name: { type: String, required: true, maxlength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
   }
 );
 
 // Virtual for author's full name
 AuthorSchema
-.virtual('name')
-.get(function () {
+  .virtual('name')
+  .get(function () {
 
-// To avoid errors in cases where an author does not have either a family name or first name
-// We want to make sure we handle the exception by returning an empty string for that case
+    // To avoid errors in cases where an author does not have either a family name or first name
+    // We want to make sure we handle the exception by returning an empty string for that case
 
-  var fullname = '';
-  if (this.first_name && this.family_name) {
-    fullname = this.family_name + ', ' + this.first_name
-  }
-  if (!this.first_name || !this.family_name) {
-    fullname = '';
-  }
+    var fullname = '';
+    if (this.first_name && this.family_name) {
+      fullname = this.family_name + ', ' + this.first_name
+    }
+    if (!this.first_name || !this.family_name) {
+      fullname = '';
+    }
 
-  return fullname;
-});
+    return fullname;
+  });
 
 // Virtual for author's lifespan
 AuthorSchema
-.virtual('lifespan')
-.get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-});
+  .virtual('lifespan')
+  .get(function () {
+
+    function datePretify(date) {
+      return moment(date).format('MMMM Do, YYYY').toString()
+    }
+    
+
+    if (this.date_of_birth && this.date_of_death) {
+      return datePretify(this.date_of_birth) + " - " + datePretify(this.date_of_death);
+    }
+    if (this.date_of_birth && !this.date_of_death) {
+      return datePretify(this.date_of_birth) + " - ";
+    }
+    if (!this.date_of_birth && this.date_of_death) {
+      return " - " + datePretify(this.date_of_death);
+    }
+
+    return " - ";
+  });
 
 // Virtual for author's URL
 AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return '/catalog/author/' + this._id;
+  });
+
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
